ec3a5305c34eb6e7436780015fcf34deb2158bfd
gordonw1271 <gordonwong1271@gmail.com>
2022-11-28 12:30:33 -0500

    assignment 3

---
+++ gyw3_MenuManager_v3/src/gyw3_MenuManager_v3/MenuTester.java
@@ -0,0 +1,13 @@
+package gyw3_MenuManager_v3;
+
+/** Class MenuTest
+ *  author : Gordon Wong
+ *  created: 11/4/2022
+ */
+public class MenuTester {
+
+	public static void main(String[] args) {
+		FileManager.readItems("data/dishes.txt");
+	}
+
+}
---
+++ gyw3_MenuManager_v3/src/gyw3_MenuManager_v3/Side.java
@@ -0,0 +1,15 @@
+package gyw3_MenuManager_v3;
+
+/** Class Side
+ *  author : Gordon Wong
+ *  created: 11/4/2022
+ */
+
+public class Side extends MenuItem{
+	
+// Constructors
+	
+	public Side(String name, String description, int calories,double price) {
+		super(name,description,calories,price);
+	}
+}
